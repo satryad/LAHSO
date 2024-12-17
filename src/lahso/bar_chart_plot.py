65861eee46c234b0b39c444e60440348a544da8e
Jeff Smits <j.smits-1@tudelft.nl>
2024-12-17 15:23:28 +0100

    Results comparison. Refactors bar_chart_plot

--- src/lahso/bar_chart_plot.py
+++ src/lahso/bar_chart_plot.py
@@ -1,66 +1,85 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# change to "Storage Cost" for the next plot,
 cost = "Cost"
-disruption = "3"
 path = "csv_output"
-simulations = 100
+simulations = 1
 
-df_aw = pd.read_csv(f"{path}/aw_{disruption}_{simulations}.csv")
-df_gp = pd.read_csv(f"{path}/gp_{disruption}_{simulations}.csv")
-df_gp.rename(columns={"Total Cost": "Total Cost gp"}, inplace=True)
-df_gp.rename(columns={"Total Storage Cost": "Total Storage Cost gp"}, inplace=True)
-df_gp.rename(columns={"Total Travel Cost": "Total Travel Cost gp"}, inplace=True)
-df_gp.rename(columns={"Total Handling Cost": "Total Handling Cost gp"}, inplace=True)
-df_gp.rename(columns={"Total Delay Penalty": "Total Delay Penalty gp"}, inplace=True)
-df_comparison = pd.merge(
-    df_aw,
-    df_gp[
-        [
-            "Episode",
-            "Total Cost gp",
-            "Total Storage Cost gp",
-            "Total Travel Cost gp",
-            "Total Handling Cost gp",
-            "Total Delay Penalty gp",
-        ]
-    ],
-    on="Episode",
-    how="left",
-)
-df_comparison["Delta"] = (
-    df_comparison[f"Total {cost}"] - df_comparison[f"Total {cost} gp"]
-)
-df_comparison.sort_values(by="Delta", ascending=False, inplace=True)
-df_comparison = df_comparison.reset_index()
 
-# Bar chart for Delta
-plt.figure(figsize=(10, 6))
+def column_color(delta, label1, label2):
+    return label1 if delta > 0 else label2
 
-# Assign colors based on the Delta values
-colors = ["blue" if delta > 0 else "orange" for delta in df_comparison["Delta"]]
 
-plt.bar(df_comparison.index, df_comparison["Delta"], color=colors)
-# plt.bar(df_comparison['Episode'], df_comparison['Delta'], color=colors)
+def comparison(file1, file2, label1, label2):
+    df1 = pd.read_csv(file1)
+    df2 = pd.read_csv(file2)
+    df_comparison = df1.copy()
+    df_comparison["Total Cost Delta"] = df1["Total Cost"] - df2["Total Cost"]
+    df_comparison["Total Storage Cost Delta"] = (
+        df1["Total Storage Cost"] - df2["Total Storage Cost"]
+    )
+    df_comparison["Total Travel Cost Delta"] = (
+        df1["Total Travel Cost"] - df2["Total Travel Cost"]
+    )
+    df_comparison["Total Handling Cost Delta"] = (
+        df1["Total Handling Cost"] - df2["Total Handling Cost"]
+    )
+    df_comparison["Total Delay Penalty Delta"] = (
+        df1["Total Delay Penalty"] - df2["Total Delay Penalty"]
+    )
+    df_comparison["Total Cost Delta Sign"] = df_comparison["Total Cost Delta"].map(
+        lambda d: column_color(d, label1, label2)
+    )
+    df_comparison["Total Storage Cost Delta Sign"] = df_comparison[
+        "Total Storage Cost Delta"
+    ].map(lambda d: column_color(d, label1, label2))
+    df_comparison["Total Travel Cost Delta Sign"] = df_comparison[
+        "Total Travel Cost Delta"
+    ].map(lambda d: column_color(d, label1, label2))
+    df_comparison["Total Handling Cost Delta Sign"] = df_comparison[
+        "Total Handling Cost Delta"
+    ].map(lambda d: column_color(d, label1, label2))
+    df_comparison["Total Delay Penalty Delta Sign"] = df_comparison[
+        "Total Delay Penalty Delta"
+    ].map(lambda d: column_color(d, label1, label2))
+    return df_comparison.reset_index()
 
-# Add a horizontal line at y=0
-plt.axhline(0, color="black", linewidth=1, linestyle="--")
 
-plt.xlabel("Episode")
-plt.ylabel(f"Total {cost} Difference")
-plt.xticks(rotation=90)
+def main():
+    df_comparison = comparison(
+        f"{path}/aw_Def_{simulations}.csv",
+        f"{path}/gp_Def_{simulations}.csv",
+        "blue",
+        "orange",
+    )
+    df_comparison.sort_values(by=f"Total {cost}", ascending=False, inplace=True)
 
-# Create custom legend
-blue_patch = plt.Line2D([0], [0], color="blue", lw=4, label="RL-Assisted")
-orange_patch = plt.Line2D([0], [0], color="orange", lw=4, label="Always Wait")
-plt.legend(handles=[blue_patch, orange_patch])
+    # Bar chart for Delta
+    plt.figure(figsize=(10, 6))
 
-min_y_limit = -60000
-max_y_limit = 60000  # Adjust the multiplier for padding
-plt.ylim(min_y_limit, max_y_limit)
+    # Assign colors based on the Delta values
+    colors = df_comparison[f"Total {cost} Delta Sign"].to_list()
 
-plt.tight_layout()
-plt.show()
+    plt.bar(df_comparison.index, df_comparison[f"Total {cost} Delta"], color=colors)
+    # plt.bar(df_comparison['Episode'], df_comparison['Delta'], color=colors)
 
-print(df_comparison.head())
+    # Add a horizontal line at y=0
+    plt.axhline(0, color="black", linewidth=1, linestyle="--")
+
+    plt.xlabel("Episode")
+    plt.ylabel(f"Total {cost} Difference")
+    plt.xticks(rotation=90)
+
+    # Create custom legend
+    blue_patch = plt.Line2D([0], [0], color="blue", lw=4, label="RL-Assisted")
+    orange_patch = plt.Line2D([0], [0], color="orange", lw=4, label="Always Wait")
+    plt.legend(handles=[blue_patch, orange_patch])
+
+    min_y_limit = -60000
+    max_y_limit = 60000  # Adjust the multiplier for padding
+    plt.ylim(min_y_limit, max_y_limit)
+
+    plt.tight_layout()
+    plt.show()
+
+    print(df_comparison.head())
--- src/lahso/ui/result_comparison_tabs.py
+++ src/lahso/ui/result_comparison_tabs.py
@@ -1,4 +1,27 @@
 import gradio as gr
+import pandas as pd
+
+from lahso.bar_chart_plot import comparison
+
+
+def check_inputs(file1, file1_label, file2, file2_label):
+    if file1 is None or file1_label is None or file2 is None or file2_label is None:
+        return gr.Tab(interactive=False)
+    return gr.Tab(interactive=True)
+
+
+def compare_results(file1, file1_label, file2, file2_label):
+    df_comparison = comparison(file1, file2, file1_label, file2_label)
+    color_map = {}
+    color_map[file1_label] = "blue"
+    color_map[file2_label] = "orange"
+    return (
+        gr.BarPlot(value=df_comparison, color_map=color_map),
+        gr.BarPlot(value=df_comparison, color_map=color_map),
+        gr.BarPlot(value=df_comparison, color_map=color_map),
+        gr.BarPlot(value=df_comparison, color_map=color_map),
+        gr.BarPlot(value=df_comparison, color_map=color_map),
+    )
 
 
 # Nested Tabs for Results Comparison
@@ -8,43 +31,97 @@ def results_comparison_tabs():
             gr.Markdown("## Output Files")
             with gr.Row():
                 with gr.Column():
-                    _file1_input = gr.File(
+                    file1_input = gr.File(
                         show_label=False,
                         file_types=[".csv"],
                         type="filepath",
                         height=100,
                     )
                 with gr.Column():
-                    _file1_label_input = gr.Textbox(
+                    file1_label_input = gr.Textbox(
                         label="Label In Plot",
                         value="Greedy Policy",
                     )
             with gr.Row():
                 with gr.Column():
-                    _file2_input = gr.File(
+                    file2_input = gr.File(
                         show_label=False,
                         file_types=[".csv"],
                         type="filepath",
                         height=100,
                     )
                 with gr.Column():
-                    _file2_label_input = gr.Textbox(
+                    file2_label_input = gr.Textbox(
                         label="Label In Plot",
                         value="Always Wait",
                     )
 
-        with gr.Tab("Policy Comparison"):
-            with gr.Row():
+        with gr.Tab("Policy Comparison", interactive=False) as comparison_tab:
+            with gr.Row(equal_height=True):
                 with gr.Column():
                     gr.Markdown("## Comparison For Each Cost Parameter")
                     with gr.Row():
-                        gr.Image(interactive=False, show_label=False)
-                        gr.Image(interactive=False, show_label=False)
+                        storage_cost_plot = gr.BarPlot(
+                            pd.DataFrame(),
+                            x="Episode",
+                            y="Total Storage Cost Delta",
+                            color="Total Storage Cost Delta Sign",
+                            color_title="",
+                        )
+                        delay_penalty_plot = gr.BarPlot(
+                            pd.DataFrame(),
+                            x="Episode",
+                            y="Total Delay Penalty Delta",
+                            color="Total Delay Penalty Delta Sign",
+                            color_title="",
+                        )
                     with gr.Row():
-                        gr.Image(interactive=False, show_label=False)
-                        gr.Image(interactive=False, show_label=False)
+                        handling_cost_plot = gr.BarPlot(
+                            pd.DataFrame(),
+                            x="Episode",
+                            y="Total Handling Cost Delta",
+                            color="Total Handling Cost Delta Sign",
+                            color_title="",
+                        )
+                        travel_cost_plot = gr.BarPlot(
+                            pd.DataFrame(),
+                            x="Episode",
+                            y="Total Travel Cost Delta",
+                            color="Total Travel Cost Delta Sign",
+                            color_title="",
+                        )
                 with gr.Column():
                     gr.Markdown("## Total Cost For Each Simulation Episode")
-                    gr.Image(interactive=False, show_label=False)
+                    total_cost_plot = gr.BarPlot(
+                        pd.DataFrame(),
+                        x="Episode",
+                        y="Total Cost Delta",
+                        color="Total Cost Delta Sign",
+                        color_title="",
+                    )
+
+        gr.on(
+            triggers=[
+                file1_input.change,
+                file1_label_input.change,
+                file2_input.change,
+                file2_label_input.change,
+            ],
+            fn=check_inputs,
+            inputs=[file1_input, file1_label_input, file2_input, file2_label_input],
+            outputs=[comparison_tab],
+        )
+
+        comparison_tab.select(
+            fn=compare_results,
+            inputs=[file1_input, file1_label_input, file2_input, file2_label_input],
+            outputs=[
+                storage_cost_plot,
+                delay_penalty_plot,
+                handling_cost_plot,
+                travel_cost_plot,
+                total_cost_plot,
+            ],
+        )
 
     return tabs
